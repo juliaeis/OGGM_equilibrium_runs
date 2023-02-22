e5af787b7071d635a14dce6f44520fc1a1a09a00
Julia Eis <jeis@uni-bremen.de>
2023-02-22 11:13:35 +0100

    update files

--- merge_output_region.py
+++ merge_output_region.py
@@ -7,11 +7,11 @@ if __name__ == '__main__':
 
     WORKING_DIR = os.environ.get("WORKDIR")
     READING_DIR = os.environ.get("READDIR")
+    REGION = str(os.environ.get('REGION')).zfill(2)
 
-#    for REGION in [dir.split('_')[-1] for dir in os.listdir(READING_DIR) if dir.startswith('region_')]:
-    for REGION in ['02','15']:
-        read_dir = os.path.join(READING_DIR, 'region_'+REGION,'*.nc')
-        print('start region '+REGION)
-        ds = xr.open_mfdataset(read_dir, parallel=True)
-        ds.to_netcdf(os.path.join(WORKING_DIR, REGION+'_equilibrium.nc'))
-        print('region '+REGION+' DONE')
+    read_dir = os.path.join(READING_DIR, 'region_'+REGION,'*.nc')
+    print('start region '+REGION)
+    ds = xr.open_mfdataset(read_dir, parallel=True)
+    ds.to_netcdf(os.path.join(WORKING_DIR, REGION+'_equilibrium.nc'))
+    print('region '+REGION+' DONE')
+    
